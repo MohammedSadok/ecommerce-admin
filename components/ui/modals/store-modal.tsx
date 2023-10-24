bdaba23043af56b755b5a934ca527f9f69b9b3a9
MohammedSadok <mohammed.sadok.2003@gmail.com>
2023-10-24 22:28:19 +0100

    add prisma

--- components/ui/modals/store-modal.tsx
+++ components/ui/modals/store-modal.tsx
@@ -40,15 +40,15 @@ export const StoreModal = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    // try {
-    //   setLoading(true);
-    //   const response = await axios.post("/api/stores", values);
-    //   window.location.assign(`/${response.data.id}`);
-    // } catch (error) {
-    //   toast.error("Something went wrong");
-    // } finally {
-    //   setLoading(false);
-    // }
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/stores", values);
+      window.location.assign(`/${response.data.id}`);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
